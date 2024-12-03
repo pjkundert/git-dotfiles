336f64a08104aaabcd2629163594f49bf3fe3683
Perry Kundert <perry@dominionrnd.com>
2024-12-03 12:06:31 -0700

    Add support for Emacs + modules, toward support for org-mode Latex

---
+++ emacs.nix
@@ -0,0 +1,52 @@
+/*
+This is a nix expression to build Emacs and some Emacs packages I like
+from source on any distribution where Nix is installed. This will install
+all the dependencies from the nixpkgs repository and build the binary files
+without interfering with the host distribution.
+
+To build the project, type the following from the current directory:
+
+$ nix-build emacs.nix
+
+To run the newly compiled executable:
+
+$ ./result/bin/emacs
+*/
+
+# The first non-comment line in this file indicates that
+# the whole file represents a function.
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  # The let expression below defines a myEmacs binding pointing to the
+  # current stable version of Emacs. This binding is here to separate
+  # the choice of the Emacs binary from the specification of the
+  # required packages.
+  myEmacs = pkgs.emacs-nox;
+  # This generates an emacsWithPackages function. It takes a single
+  # argument: a function from a package set to a list of packages
+  # (the packages that will be available in Emacs).
+  emacsWithPackages = (pkgs.emacsPackagesFor myEmacs).emacsWithPackages;
+in
+  # The rest of the file specifies the list of packages to install. In the
+  # example, two packages (magit and zerodark-theme) are taken from
+  # MELPA stable.
+  emacsWithPackages (epkgs: (with epkgs.melpaStablePackages; [
+    magit          # ; Integrate git <C-x g>
+    #zerodark-theme # ; Nicolas' theme
+  ])
+  # Two packages (undo-tree and zoom-frm) are taken from MELPA.
+  ++ (with epkgs.melpaPackages; [
+    #undo-tree      # ; <C-x u> to show the undo tree
+    #zoom-frm       # ; increase/decrease font size for all buffers %lt;C-x C-+>
+  ])
+  # Three packages are taken from GNU ELPA.
+  ++ (with epkgs.elpaPackages; [
+    #auctex         # ; LaTeX mode
+    #beacon         # ; highlight my cursor when scrolling
+    #nameless       # ; hide current package name everywhere in elisp code
+  ])
+  # notmuch is taken from a nixpkgs derivation which contains an Emacs mode.
+  ++ [
+    #pkgs.notmuch   # From main packages set
+  ])
--- env.nix
+++ env.nix
@@ -1,13 +1,25 @@
-with import <nixpkgs> {}; [
-  # Development tools
-  emacs
+let
+  pkgs = import <nixpkgs> {};
+  customEmacs = import ./emacs.nix { inherit pkgs; };
+in with pkgs; [  
+  # Editors
+  customEmacs
   ispell
+
+  # Development tools
+  cacert
+  git
   gnumake
-  screen
   jq
+  openssh
+  screen
 
   # Python 3 default
   python312Full
   python312Packages.pytest
   python312Packages.tkinter
+  python312Packages.jupyterlab
+  python312Packages.numpy
+  python312Packages.scipy
+  python312Packages.scikitlearn
 ]
